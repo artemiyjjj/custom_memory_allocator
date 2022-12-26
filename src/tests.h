851a43e09a9a82df1043e5334a2378c6d5b1b814
artemioJjJ <artemiyromanoff@gmail.com>
2022-12-26 19:24:51 +0300

    Added all tests

---
+++ src/tests.h
@@ -0,0 +1,10 @@
+#ifndef MEMORY_ALLOCATOR_TESTS_H
+#define MEMORY_ALLOCATOR_TESTS_H
+
+#include "tests/includes/test1.h"
+#include "tests/includes/test2.h"
+#include "tests/includes/test3.h"
+#include "tests/includes/test4.h"
+#include "tests/includes/test5.h"
+
+#endif //MEMORY_ALLOCATOR_TESTS_H
